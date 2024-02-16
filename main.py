87b15f8725da4c3f826684da9a62cd01802f1745
Hnat Liashenko <hnat.liashenko@bluepes.com>
2024-02-16 21:33:20 +0200

    init project

---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import telebot
+from time import sleep
+from dotenv import load_dotenv
+from pytube import YouTube
+
+load_dotenv()
+
+TOKEN = os.environ["TOKEN"]
+
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+
+def get_chat_id(message):
+    return message.chat.id
+
+
+@bot.message_handler(commands=["start"])
+def handle_start_command(message):
+    chat_id = get_chat_id(message)
+
+    bot.send_message(
+        chat_id,
+        "Привіт! 👋\nНадішли посилання на пісню з YouTube або YouTube Music та я скачаю її для тебе.\n\n Я працюю дуже швидко та без реклами 🤝\n\n За наявності питань та/або проблем з ботом звертатись: @gnatuk2312",
+    )
+
+
+@bot.message_handler(commands=["help"])
+def handle_help_command(message):
+    chat_id = get_chat_id(message)
+
+    bot.send_message(
+        chat_id,
+        "Щоб почати роботу зі мною надсилай посилання на YouTube або YouTube Music, який хочеш скачати.\n\nПриклад посилання: https://music.youtube.com/watch?v=CLjKo_XkxJI&list=RDAMVMCLjKo_XkxJI",
+    )
+
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    chat_id = get_chat_id(message)
+    # send loading indicator and save in variable to delete later
+    downloading_message = bot.send_message(chat_id, "Downloading...")
+
+    try:
+        # find and download audio from YouTube in mp3 format
+        video = YouTube(message.text)
+        stream = video.streams.filter(only_audio=True).first()
+        filename = f"{video.title} - {video.author}.mp3"
+        stream.download(filename=filename, output_path="./music")
+
+        # read binary file
+        filepath = f"./music/{filename}"
+        # send audio
+        bot.send_audio(
+            chat_id, open(filepath, "rb"), title=video.title, performer=video.author
+        )
+        # delete audio
+        os.remove(filepath)
+    except Exception as error:
+        bot.send_message(
+            chat_id,
+            f"Не вдалось завантажити файл... 🤨\n\nМожливі причини:\n- Некоректне посилання \n- Відео захищено правами доступу від завантажень\n\nТекст помилки: {error}",
+        )
+    finally:
+        # delete loading indicator
+        bot.delete_message(chat_id, downloading_message.id)
+
+
+bot.polling(non_stop=True, interval=0)
